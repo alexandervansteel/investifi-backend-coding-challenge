8b23d12d4e5e5caccac39177b7d21a789309e5f3
Alex Vansteel <alexandervansteel@gmail.com>
2025-03-04 18:43:29 -0600

    code challenge

--- src/api.py
+++ src/api.py
@@ -1,4 +1,12 @@
-from fastapi import FastAPI
+import boto3
+
+from botocore.exceptions import ClientError
+from fastapi import FastAPI, HTTPException
+
+from src.model import User, RecurringOrder, RecurringOrderFrequencyEnum, RecurringOrderCurrencyEnum
+
+
+recurring_order_table = RecurringOrder.__table_name__
 
 
 app = FastAPI(
@@ -15,19 +23,37 @@ def hello_world():
     return {"hello": "world"}
 
 
-@app.get("/recurring-orders")
-def get_recurring_orders():
+@app.get("/recurring-orders/{user_id}")
+def get_recurring_orders(user_id: str):
     """
     TODO
     # Requirements:
     # The GET route should accept a User ID and return only said users recurring orders
     # if no ID is provided, an error should be raised.
     """
-    return {}
+    
+    dynamodb_client = boto3.client('dynamodb')
+
+    key_condition_expression = 'hash_key=:hash_key'
+    filter_expression = 'not contains(:range_key, range_key)'
+    expression_values = {':hash_key': {'S': f'User#{user_id}'},
+                         ':range_key': {'S': 'details'}}
+
+    dynamodb_response = dynamodb_client.query(
+        TableName=recurring_order_table,
+        KeyConditionExpression=key_condition_expression,
+        FilterExpression=filter_expression,
+        ExpressionAttributeValues=expression_values,
+    )
+    
+    if recurring_orders := dynamodb_response.get('Items'):
+        return recurring_orders
+    else:
+        raise HTTPException(status_code=404, detail=str('no recurring orders found'))
 
 
 @app.post("/recurring-orders")
-def post_recurring_orders():
+def post_recurring_orders(recurring_order: RecurringOrder):
     """
     TODO
     Requirements:
@@ -38,4 +64,26 @@ def post_recurring_orders():
     A recurring order must have a USD amount greater than 0
     A recurring order needs to be associated with a specifc user.
     """
-    return {}
+    
+    dynamodb_client = boto3.client('dynamodb')
+    
+    # low cost and fast query on index to see if recording for user with the currency and frequency pair 
+    key_condition_expression = f'hash_key=:hash_key AND contains(:range_key, {recurring_order.currency}) AND contains(:range_key, {recurring_order.frequency})'
+    expression_values = {':hash_key': {'S': recurring_order.hash_key},
+                         ':range_key': {'S': recurring_order.range_key}}
+
+    dynamodb_response = dynamodb_client.query(
+        TableName=recurring_order_table,
+        KeyConditionExpression=key_condition_expression,
+        ExpressionAttributeValues=expression_values,
+    )
+    
+    if recurring_order := dynamodb_response.get('Item'):
+      raise HTTPException(status_code=422, detail='record already exists for user and currency')
+    else:
+        try:
+            dynamodb_client.put(Item=recurring_order.dict())
+        except ClientError as e:
+            raise HTTPException(status=500, detail=str(e))
+            
+    return recurring_order
